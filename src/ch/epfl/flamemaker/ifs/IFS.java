bddf9706603db574ae9f755a385117b5f093f85a
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2013-02-19 14:56:55 +0100

    IFS computing & generating test

--- src/ch/epfl/flamemaker/ifs/IFS.java
+++ src/ch/epfl/flamemaker/ifs/IFS.java
@@ -2,6 +2,7 @@ package ch.epfl.flamemaker.ifs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import ch.epfl.flamemaker.geometry2d.*;
 
@@ -10,12 +11,29 @@ public class IFS {
 	private List<AffineTransformation> m_transforms;
 	
 	public IFS(List<AffineTransformation> transformations){
-		
 		m_transforms = new ArrayList<AffineTransformation>(transformations);
 	}
 	
 	public IFSAccumulator compute(Rectangle frame, int width, int height, int density){
-		//TODO : not yet implemented
-		return null;
+		Point point = new Point(0, 0);
+		int k = 20;
+		int transformationNum = 0;
+		Random randomizer = new Random();
+		int size = m_transforms.size();
+		IFSAccumulatorBuilder builder = new IFSAccumulatorBuilder(frame, width, height);
+				
+		for(int i = 0; i < k; i++) {
+			transformationNum = randomizer.nextInt(size);
+			point = m_transforms.get(transformationNum).transformPoint(point);
+		}
+		
+		int m = density * width * height;
+		for(int i = 0; i < m; i++) {
+			transformationNum = randomizer.nextInt(size);
+			point = m_transforms.get(transformationNum).transformPoint(point);
+			builder.hit(point);
+		}
+		
+		return builder.build();
 	}
 }
