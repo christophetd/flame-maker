f8d4430b8978b37662d9a3fb536d1c7bbbbde213
Hadrien Milano <hadrien.milano@gmail.com>
2013-02-19 09:22:14 +0100

    ajout de la classe point

--- src/ch/epfl/flamemaker/geometry2d/Point.java
+++ src/ch/epfl/flamemaker/geometry2d/Point.java
@@ -1,5 +1,33 @@
 package ch.epfl.flamemaker.geometry2d;
 
 public class Point {
-
+	
+	public static final Point ORIGIN = new Point(0,0);
+	
+	private double m_x, m_y;
+	
+	public Point(double x, double y){
+		m_x = x;
+		m_y = y;
+	}
+	
+	public double x(){
+		return m_x;
+	}
+	
+	public double y(){
+		return m_y;
+	}
+	
+	public double r(){
+		return Math.sqrt(m_x*m_x + m_y*m_y);
+	}
+	
+	public double theta(){
+		return Math.atan2(m_y, m_x);
+	}
+	
+	public String toString(){
+		return "("+m_x+","+m_y+")";
+	}
 }
