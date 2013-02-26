cf6ed1b4ee10ca17f6bb0be47b6e1f4045d9d03b
Hadrien Milano <hadrien.milano@gmail.com>
2013-02-26 08:54:09 +0100

    created flameTransformation

---
+++ src/ch/epfl/flamemaker/flame/FlameTransformation.java
@@ -0,0 +1,34 @@
+package ch.epfl.flamemaker.flame;
+
+import ch.epfl.flamemaker.geometry2d.*;
+
+public class FlameTransformation implements Transformation{
+	
+	private final AffineTransformation m_affineTransfo;
+	private final double[] m_weight;
+	
+	
+	FlameTransformation(AffineTransformation affineTransformation, double[] variationWeight) {
+		if(variationWeight.length != 6){
+			throw new IllegalArgumentException("variationWeight must have length 6");
+		}
+			
+			
+		m_affineTransfo = affineTransformation;
+		m_weight = variationWeight.clone();
+	}
+
+
+	@Override
+	public Point transformPoint(Point p) {
+		
+		Point tmp, ret = new Point(0,0);
+		
+		for(int i = 0 ; i < Variation.ALL_VARIATIONS.size() ; i++){
+			tmp = Variation.ALL_VARIATIONS.get(i).transformPoint(m_affineTransfo.transformPoint(p));
+			ret =  new Point(ret.x()+m_weight[i]*tmp.x(), ret.y()+m_weight[i]*tmp.y());
+		}
+		
+		return ret;
+	}
+}
