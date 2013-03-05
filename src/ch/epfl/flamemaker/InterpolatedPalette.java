51ba94e05d91055ee4de9f573417511ebd88c3aa
Hadrien Milano <hadrien.milano@gmail.com>
2013-03-05 09:29:56 +0100

    Added palettes

---
+++ src/ch/epfl/flamemaker/InterpolatedPalette.java
@@ -0,0 +1,13 @@
+package ch.epfl.flamemaker;
+
+import ch.epfl.flamemaker.color.Color;
+
+public class InterpolatedPalette implements Palette {
+
+	@Override
+	public Color colorForIndex(double index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ch/epfl/flamemaker/Palette.java
@@ -0,0 +1,9 @@
+package ch.epfl.flamemaker;
+
+import ch.epfl.flamemaker.color.Color;
+
+public interface Palette {
+
+	Color colorForIndex(double index);
+
+}
