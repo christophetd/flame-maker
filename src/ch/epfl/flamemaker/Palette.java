303c6c5ba8f3426037e455d860bd1817ecd1a627
Hadrien Milano <hadrien.milano@gmail.com>
2013-03-19 14:10:03 +0100

    commentaires, code et indentation du package flamemaker et flamemaker.color

--- src/ch/epfl/flamemaker/Palette.java
+++ src/ch/epfl/flamemaker/Palette.java
@@ -13,6 +13,7 @@ public interface Palette {
 	 * Récupère la couleur de la palette pour un index donné
 	 * @param index correspondant à la couleur voulue compris entre 0 et 1
 	 * @return instance de la couleur correspondant à l'index
+	 * @throws IllegalArgumentException si l'index n'est pas dans l'intervalle [0, 1]
 	 */
 	Color colorForIndex(double index);
 
--- src/ch/epfl/flamemaker/color/RandomPalette.java
+++ src/ch/epfl/flamemaker/color/RandomPalette.java
@@ -14,27 +14,32 @@ import ch.epfl.flamemaker.Palette;
 public class RandomPalette implements Palette{
 
 	InterpolatedPalette m_palette;
-	
+
+	/**
+	 * Construit une nouvelle palette aléatoire avec le nombre de couleurs spécifié
+	 * @param nbColors nombre de couleurs choisies aléatoirement
+	 */
 	public RandomPalette(int nbColors){
 		if(nbColors < 2)
 			throw new IllegalArgumentException("Must have at least 2 colors");
-		
+
 		List<Color> colors = new ArrayList<Color>();
-		
+
 		for(int i = 0 ; i < nbColors ; i++){
 			colors.add(new Color(Math.random(), Math.random(), Math.random()));
 		}
-		
+
 		m_palette = new InterpolatedPalette(colors);
 	}
 
 	/**
 	 * Calcule une interpolation des couleurs sur la palette aléatoire.
 	 * @return couleur calculée
+	 * @throws IllegalArgumentException lorsque l'index n'est pas dans l'intervalle [0, 1]
 	 */
 	@Override
 	public Color colorForIndex(double index) {
 		return m_palette.colorForIndex(index);
 	}
-	
+
 }
