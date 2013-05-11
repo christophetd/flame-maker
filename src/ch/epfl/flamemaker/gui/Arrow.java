592495e82a05da5daa8ebca58405a1b8ce44c3f7
Hadrien Milano <hadrien.milano@gmail.com>
2013-05-11 15:54:33 +0200

    revert to dirty ugly arrows ;(

--- src/ch/epfl/flamemaker/gui/Arrow.java
+++ src/ch/epfl/flamemaker/gui/Arrow.java
@@ -7,39 +7,19 @@ import ch.epfl.flamemaker.geometry2d.Point;
 import ch.epfl.flamemaker.geometry2d.Transformation;
 
 public class Arrow {
-	private static final double H = 0.0707;
-	private Point m_from = new Point(-1, 0);
-	private Point m_to = new Point(1, 0);
+	private Point m_from;
+	private Point m_to;
 	private Point m_leftComponent;
 	private Point m_rightComponent;
 	public Arrow(Point from, Point to) {
 		m_from = from;
 		m_to = to;
-		buildComponents();
+		m_leftComponent = new Point(to.x()-0.1, to.y()+0.1);
+		m_rightComponent = new Point(to.x()-0.1, to.y()-0.1);
 	}
 	
-	private void buildComponents() {
-		double dx = m_to.x() - m_from.x();
-		double dy = m_to.y() - m_from.y();
-
-		double l = Math.sqrt(dx*dx + dy*dy);
-		double alpha = Math.atan2(dx, dy);
-
-		double beta = alpha - Math.PI/4;
-		
-		
-		double x0 = l*H*Math.sin(beta);
-		double y0 = l*H*Math.cos(beta);
-		
-		double x1 = l*H*(Math.sin(beta + Math.PI/2));
-		double y1 = l*H*(Math.cos(beta + Math.PI/2));
-		
-		m_leftComponent = new Point(m_to.x() - x0, m_to.y() - y0);
-		m_rightComponent = new Point(m_to.x() - x1, m_to.y() - y1);
-	}
 	
 	public void draw(Graphics2D g, Transformation mapper) {
-		buildComponents();
 		m_from = mapper.transformPoint(m_from);
 		m_to = mapper.transformPoint(m_to);
 		m_leftComponent = mapper.transformPoint(m_leftComponent);
@@ -51,6 +31,11 @@ public class Arrow {
 	}
 	
 	public Arrow applyTransformation(Transformation transfo) {
-		return new Arrow(transfo.transformPoint(m_from),transfo.transformPoint(m_to));
+		m_from = transfo.transformPoint(m_from);
+		m_to = transfo.transformPoint(m_to);
+		m_leftComponent = transfo.transformPoint(m_leftComponent);
+		m_rightComponent = transfo.transformPoint(m_rightComponent);
+		
+		return this;
 	}
 }
