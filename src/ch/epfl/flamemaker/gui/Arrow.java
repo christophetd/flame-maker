a1610f7a64960273d88c74e1fbcb9c0ff55b3645
Hadrien Milano <hadrien.milano@gmail.com>
2013-05-12 17:53:47 +0200

    reusing badass trigo for nice clean arrows

--- src/ch/epfl/flamemaker/gui/Arrow.java
+++ src/ch/epfl/flamemaker/gui/Arrow.java
@@ -7,6 +7,7 @@ import ch.epfl.flamemaker.geometry2d.Point;
 import ch.epfl.flamemaker.geometry2d.Transformation;
 
 public class Arrow {
+	private double H = 0.0707;
 	private Point m_from;
 	private Point m_to;
 	private Point m_leftComponent;
@@ -15,10 +16,29 @@ public class Arrow {
 	public Arrow(Point from, Point to) {
 		m_from = from;
 		m_to = to;
-		m_leftComponent = new Point(to.x()-0.1, to.y()+0.1);
-		m_rightComponent = new Point(to.x()-0.1, to.y()-0.1);
+		
+		buildComponents();
 	}
 	
+	private void buildComponents() {
+		double dx = m_to.x() - m_from.x();
+		double dy = m_to.y() - m_from.y();
+
+		double l = Math.sqrt(dx*dx + dy*dy);
+		double alpha = Math.atan2(dx, dy);
+
+		double beta = alpha - Math.PI/4;
+		
+		
+		double x0 = l*H*Math.sin(beta);
+		double y0 = l*H*Math.cos(beta);
+		
+		double x1 = l*H*(Math.sin(beta + Math.PI/2));
+		double y1 = l*H*(Math.cos(beta + Math.PI/2));
+		
+		m_leftComponent = new Point(m_to.x() - x0, m_to.y() - y0);
+		m_rightComponent = new Point(m_to.x() - x1, m_to.y() - y1);
+	}
 	
 	public void draw(Graphics2D g) {
 		
